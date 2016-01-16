177cf34c92ec1afe71963eaf8672e80b3258fc23
Vetri <vairamut@usc.edu>
2016-01-16 12:00:23 +0530

    first commit

---
+++ FastAndSlow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FastAndSlow
+//
+//  Created by Vetri Selvi Vairamuthu on 1/16/16.
+//  Copyright © 2016 Vetri Selvi Vairamuthu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
